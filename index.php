4e657a897b5349234a25facb68a643a5e9261443
ArilCyber <121453807+ArilCyber@users.noreply.github.com>
2023-10-30 08:01:24 +0700

    sssw
    swswsw

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+
+<html>
+
+  <head>
+    <title>Hacked by Iman19X</title>
+    <script src="" type="text/javascript"></script>
+  </head>
+<body bgcolor="black">
+<center>
+  <font color="white" face="Courier New" size="3">
+  <marquee bgcolor="red"><b>Greetz To : 
+ Manusia Biasa Team- SukowonoBlackhatTeam- Esteem Restoration Eagle
+- From Lammer To Mastah
+- Clan Akatsuki Team
+- Always Alone BlackHat
+- Jakarta Cyber Ghost
+- Jakarta Security Cyber
+- Jakarta Blackhat Cyber Crime
+- Kristen Zone Hacking
+- Angkatan Militer Security
+- M.C.S.I
+- Cyber JatengXploit
+- Indonesia Xploiter
+- Anonymous Hacktivist Team
+- Panglima Cyber Squad 2018
+- GARUDA INDONESIA
+- Garuda BlackHat
+- Faster BlackHat
+- Bandung Defacer.ID
+- Bondowoso BlackHat Team 
+- 3Xploit.ID
+- PirateSecurity
+- InfectionSystem
+- K3B0L3DAN CYBER CRIME
+</AND ALL TEAMxMEMBER></b></marquee></font><br>
+  <img width="350" src="https://demo.tunedperfection.com//wp-content/plugins/wp-symposium/server/php/Screenshot_2023-10-18-19-18-50-707-edit_com.lemon.lvoverseas.jpg"></br>
+  <font color="red" face="Courier New" size="5"><b></br>< ! > Hacked by <font color="red">Iman19X < ! ></b><font size="4"><br>Gabuters Crew Was Here!!</font><br><br><font color="white" face="Courier New" size="6">DASAR INDIA TOLOL!!!</br></font><font color="white" face="Courier New" size="5"><br>क्या आप हमारे धर्म का सम्मान कर सकते हैं? आपको हमारे धर्म, इस्लाम का अपमान करने से क्या मिला? <br>क्या आप हमारे धर्म को सहन करने और सम्मान करने के लिए थोड़े ही हो सकते हैं? <br>कृपया हमारे धर्म का अपमान करना बंद करें, या बुरी चीजें आपके साथ होंगी!!<br>____________________<br>
+<font color="red" face="Courier New" size="5">INDO<font color="white" face="Courier New" size="5">NESIA</br>
+<font color="blue" face="Courier New" size="5">HACKTIVIST!!<br><br></font><font color="blue" face="Courier New" size="3"> #SavePalestine#FresPalestina #StopInsultingIslamic</br>
